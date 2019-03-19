d4f9ccffdcf6eaf719a594ae92094972c1fc14df
Jpsern <tommy999_jps@yahoo.co.jp>
2019-03-19 15:40:51 +0900

    Qiita の通知数表示をいじって遊ぶスクリプトを作成
    ネタスクリプトです。

---
+++ qiita-notification-count-changer.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         qiita-notification-count-changer
+// @namespace    None
+// @version      0.1
+// @description  通知カウント欄の表示を変える
+// @author       Jpsern
+// @match        https://qiita.com/*
+// ==/UserScript==
+
+(() => {
+  const notifyElem = document.querySelector(".st-Header_notifications");
+
+  const main = () => {
+    //create count-up button
+    const countUpButton = createButton();
+    countUpButton.textContent = "+";
+    countUpButton.onclick = () => countUp();
+
+    //create reset button
+    const resetButton = createButton();
+    resetButton.textContent = "C";
+    resetButton.onclick = () => reset();
+
+    //set buttons
+    const targetWrapper = document.getElementsByClassName("st-Header_end")[0];
+    targetWrapper.insertBefore(resetButton, notifyElem);
+    targetWrapper.insertBefore(countUpButton, notifyElem);
+  }
+
+  const createButton = () => {
+    const button = document.createElement("button");
+    button.classList.add("px-2", "mr-2");
+    button.style.display = "flex";
+    button.style.alignItems = "center";
+    button.style.justifyContent = "center";
+    button.style.width = "32px";
+    button.style.height = "32px";
+    button.style.backgroundColor = "#3f9200";
+    button.style.color = "#fff";
+    button.style.borderRadius = "4px";
+    button.style.fontSize = "1.2rem";
+    button.style.position = "relative";
+    button.style.cursor = "pointer";
+
+    return button;
+  }
+
+  const countUp = () => {
+    console.log("up");
+    notifyElem.classList.add("st-Header_notifications-active");
+    notifyElem.innerText = parseInt(notifyElem.innerText, 10) + 1;
+  }
+
+  const reset = () => {
+    notifyElem.classList.remove("st-Header_notifications-active");
+    notifyElem.innerText = 0;
+  }
+
+  main();
+})();
